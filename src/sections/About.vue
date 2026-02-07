05141d1ece7de164af1bcfdd73f788e9729a6262
Rexsaa <frankandrewwzz@gmail.com>
2026-02-07 07:47:05 +0700

    Update About.vue

--- src/sections/About.vue
+++ src/sections/About.vue
@@ -2,15 +2,15 @@
   <section id="about" class="py-24 px-6 bg-white">
     <div class="max-w-4xl mx-auto grid md:grid-cols-2 gap-12 items-center">
       <div class="aspect-square bg-slate-200 rounded-2xl overflow-hidden grayscale hover:grayscale-0 transition duration-500">
-        <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=800&q=80" alt="Profile" class="w-full h-full object-cover">
+        <img src="https://ibb.co.com/gZ6DCw4f" alt="ini elgaa" class="w-full h-full object-cover">
       </div>
       <div>
         <h3 class="text-3xl font-bold mb-6 text-slate-900">Tentang Saya</h3>
         <p class="text-slate-600 mb-4 leading-relaxed">
-          Saya adalah seorang pengembang perangkat lunak yang berfokus pada pembuatan website yang tidak hanya terlihat bagus, tetapi juga berperforma tinggi.
+          Saya adalah macan.
         </p>
         <p class="text-slate-600 leading-relaxed">
-          Dengan pengalaman lebih dari 3 tahun, saya senang memecahkan masalah kompleks melalui kode yang bersih dan terstruktur. Saya percaya bahwa teknologi harus memudahkan hidup manusia.
+          Dengan taring yang imoet.
         </p>
       </div>
     </div>
