f4631d47d05580be8ec44cd3692e82b629215622
Rexsaa <frankandrewwzz@gmail.com>
2026-02-07 07:17:10 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url
+import { fileURLToPath, URL } from 'node:url'
+
 export default defineConfig({
-  base: '/porto/', 
-  
+  base: '/porto/',
   plugins: [vue()],
-  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-
-  server: {
-    port: 3000,
-    open: true
-  },
-
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    minify: 'terser',
-    sourcemap: false
   }
 })
