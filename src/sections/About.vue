5d6e56bbd575139e69662fb3ad252f1f171d2845
Rexsaa <frankandrewwzz@gmail.com>
2026-02-07 07:52:32 +0700

    Update About.vue

--- src/sections/About.vue
+++ src/sections/About.vue
@@ -2,7 +2,7 @@
   <section id="about" class="py-24 px-6 bg-white">
     <div class="max-w-4xl mx-auto grid md:grid-cols-2 gap-12 items-center">
       <div class="aspect-square bg-slate-200 rounded-2xl overflow-hidden grayscale hover:grayscale-0 transition duration-500">
-        <img src="https://i.ibb.co.com/CKWnFvPY/IMG-0386.jpg" alt="ini elgaa" class="w-full h-full object-cover">
+        <img src="https://i.ibb.co.com/RT7CWPjs/IMG-0386.jpg" alt="ini elgaa" class="w-full h-full object-cover">
       </div>
       <div>
         <h3 class="text-3xl font-bold mb-6 text-slate-900">Tentang Saya</h3>
