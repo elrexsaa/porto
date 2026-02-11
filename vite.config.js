580b005c81a29bc81d305233685cfc28c276bbf6
Rexsaa <frankandrewwzz@gmail.com>
2026-02-12 05:02:02 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  // Base path sesuai nama repo kamu
+  // Base path sesuai nama repository GitHub kamu
   base: '/porto/', 
   
   plugins: [vue()],
@@ -12,5 +12,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  // Build config dibuat simpel karena kita tidak pakai library Google AI lagi
+  build: {
+    outDir: 'dist',
+    minify: 'terser', // Opsional: biar file hasil build lebih kecil & kencang
   }
 })
